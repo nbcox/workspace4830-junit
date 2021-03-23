350a894f3277265587c9e95b9799c97b8296f661
N Cox <nbcox@unomaha.edu>
2021-03-22 23:41:16 -0500

    remove extra examples

--- simple-junit-examples/src/p2/parameterization/Customer.java
+++
@@ -1,9 +0,0 @@
-package p2.parameterization;
-
-public class Customer {
-   public int getValue(int i1, int i2) {
-      int value = 0;
-      value = i1 + i2;
-      return value;
-   }
-}
--- simple-junit-examples/src/p4/csvfilesource/Customer.java
+++
@@ -1,9 +0,0 @@
-package p4.csvfilesource;
-
-public class Customer {
-   public int getValue(int i1, int i2) {
-      int value = 0;
-      value = i1 + i2;
-      return value;
-   }
-}
